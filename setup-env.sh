a1e205e50466ac95b4d6281bb71bc4bb60bd00b9
Ian Mancini <ianmethyst@gmail.com>
2021-10-27 02:40:45 -0300

    Improve setup scripts

--- setup-env.sh
+++ setup-env.sh
@@ -15,25 +15,27 @@ gen_key() {
   uuidgen
 }
 
-
 gen_secret() {
   check_available openssl
   openssl rand -base64 24
 }
 
-read -p "Directus key: (press enter to randomize): " directus_key
-directus_key=${directus_key:-`gen_key`}
-
-read -p "Directus secret: (press enter to randomize): " directus_secret
-directus_secret=${directus_secret:-`gen_secret`}
-
+echo "Setting up .env"
 if [ ! -f "./.env" ]; then
+  read -p "Directus key: (press enter to randomize): " directus_key
+  directus_key=${directus_key:-`gen_key`}
+
+  read -p "Directus secret: (press enter to randomize): " directus_secret
+  directus_secret=${directus_secret:-`gen_secret`}
+
   echo "Copying ./env.example to ./.env"
   cp ./env.example ./.env
+
+  sed -i -e "s#KEY=.*#KEY=${directus_key}#g" \
+    "$(dirname "$0")/.env"
+
+  sed -i -e "s#SECRET=.*#SECRET=${directus_secret}#g" \
+    "$(dirname "$0")/.env"
+else
+  echo ".env already setup"
 fi
-
-sed -i -e "s#KEY=.*#KEY=${directus_key}#g" \
-       "$(dirname "$0")/.env"
-
-sed -i -e "s#SECRET=.*#SECRET=${directus_secret}#g" \
-       "$(dirname "$0")/.env"
